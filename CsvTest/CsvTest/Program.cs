56c761b07f3ef912cb73497a2fa7e4948a3d59cb
shuiyuan_90@qq.com <shuiyuan_90@qq.com>
2020-04-15 11:22:06 +0800

    Csv文件操作

---
+++ CsvTest/CsvTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CsvTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+            Read();
+            Console.Read();
+        }
+        public static void Read()
+        {
+            string filePathCsv = "Test.csv";
+            using (var reader = new StreamReader(filePathCsv, Encoding.GetEncoding("GB2312")))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                foreach (var item in records)
+                {
+                    Console.WriteLine(item.Name);
+                }
+            }
+        }
+        public static void Write()
+        {
+            string filePathCsv = "Test.csv";
+            var records = new List<Test>
+            {
+                new Test { Id = 10, Name = "then" },
+            };
+            using (var writer = new StreamWriter(filePathCsv,false))//覆盖模式
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+            }
+        }
+    }
+
+    class Test
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Csv文件操作  CsvHelper使用介绍
+
+[官方地址](https://joshclose.github.io/CsvHelper/)
+
+
+## 在NuGet下载 CsvHelper
+
+- 使用CsvHelp需要确保项目Net版本高于4.5
+
+![依赖](依赖.png)
+
+- 在官网有大量的使用例子
+
+- **在读取过程中如果报错误 “Bad data·····”可以使用记事本打开.csv文件查看。**在做实验的时候有发现wps创建csv文件，在wps下产看是没有问题，但是在记事本下打开是乱码的情况。
